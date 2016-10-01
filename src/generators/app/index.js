e65dffda1f80b7ac2f2c98d25dc5d3ca947f4aee
Andreas Riedl <office@tapirdata.com>
2016-10-01 22:24:31 +0200

    clean up generator code

--- src/generators/app/index.js
+++ src/generators/app/index.js
@@ -1,120 +1,190 @@
 import path from 'path'; 
+import _ from 'lodash';
 import yosay from 'yosay'; 
+import slug from 'slug';
 
 import BaseGenerator from '../../lib/base';
 
-/* eslint-disable no-console */
 
-let Generator = BaseGenerator.extend({
+class AppGenerator extends BaseGenerator { 
 
-  constructor() {
-    BaseGenerator.apply(this, arguments);
+  constructor(...args) {
+    super(...args);
     this.sourceRoot(path.join(__dirname, '../../../templates/app'));
-  },
+    this._initConfig();
+    this._optionConfig();
+    this._defaultConfig();
+  }
 
-  prompting: {
-    askFor() {
-      let { config } = this;
-      let use = config.get('use');
-      if (!this.options['skip-welcome-message']) {
-        // Have Yeoman greet the user.
-        this.log(yosay('Welcome to the marvelous ExpressDevelop generator!'));
-      }
-      let prompts = [
-        {
-          type: 'checkbox',
-          name: 'features',
-          message: 'What more would you like?',
-          choices: [
-            {
-              name: 'Modernizr',
-              value: 'modernizr',
-              checked: use.modernizr
-            },
-            {
-              name: 'Foundation',
-              value: 'foundation',
-              checked: use.foundation
-            },
-            {
-              name: 'Bootstrap',
-              value: 'bootstrap',
-              checked: use.bootstrap
-            },
-            {
-              name: 'Backbone',
-              value: 'backbone',
-              checked: use.backbone
-            },
-            {
-              name: 'Marionette',
-              value: 'marionette',
-              checked: use.marionette
-            },
-            {
-              name: 'Sass',
-              value: 'sass',
-              checked: use.sass
-            },
-            {
-              name: 'Cache-crusher',
-              value: 'crusher',
-              checked: use.crusher
-            }
-          ]
-        }
-      ];
-      // console.log "prompts=#{JSON.stringify(prompts)}"
-      return this.prompt(prompts)
-        .then(answers => {
-          let hasFeature = feat => answers.features.indexOf(feat) !== -1;
+  _initConfig() {
+    let { config } = this;
+    config.defaults({
+      appname: this.appname,
+      appnameSlug: slug(this.appname),
+      appnameCap: _.capitalize(this.appname),
+      dirs: {},
+      use: {},
+    });
+  }
 
-          // console.log "answers=#{JSON.stringify(answers)}"
-          use.modernizr = hasFeature('modernizr');
-          use.foundation = hasFeature('foundation');
-          use.bootstrap = hasFeature('bootstrap');
-          use.backbone = hasFeature('backbone');
-          use.marionette = hasFeature('marionette');
-          use.sass = hasFeature('sass');
-          use.crusher = hasFeature('crusher');
-          return config.set('use', use);
-        }
-      );
+  _optionConfig() {
+    let { config } = this;
+    let { options } = this;
+    let dirs = config.get('dirs');
+    config.set('dirs', _.defaults(dirs, {
+      bower: options['bower-dir'],
+      src: options['src-dir'],
+      serverSrc: options['server-src-dir'],
+      clientSrc: options['client-src-dir'],
+      test: options['test-dir'],
+      tmp: options['tmp-dir'],
+      tmpDev: options['tmp-dev-dir'],
+      tmpTest: options['tmp-test-dir'],
+      dist: options['dist-dir']
+    }));
+    //TODO: set 'use'
+  }
+
+  _defaultConfig() {
+    let { config } = this;
+    let dirs = undefined;
+    let use = undefined;
+    dirs = config.get('dirs');
+    config.set('dirs', _.defaults(dirs, {
+      bower: 'bower_components',
+      src: 'src',
+      test: 'test',
+      tmp: '.tmp',
+      dist: 'dist'
+    }));
+    dirs = config.get('dirs');
+    config.set('dirs', _.defaults(dirs, {
+      serverSrc: path.join(dirs.src, 'server'),
+      clientSrc: path.join(dirs.src, 'client'),
+      tmpDev: path.join(dirs.tmp, 'dev'),
+      tmpTest: path.join(dirs.tmp, 'test')
+    }));
+    use = config.get('use');
+    config.set('use', _.defaults(use, {
+      backbone: true,
+      marionette: true,
+      foundation: true,
+      bootstrap: false,
+      modernizr: true,
+      sass: true,
+      crusher: true
+    }));
+  }
+
+  prompting () {
+    let { config } = this;
+    let use = config.get('use');
+    if (!this.options['skip-welcome-message']) {
+      // Have Yeoman greet the user.
+      this.log(yosay('Welcome to the marvelous ExpressDevelop generator!'));
     }
-  },
+    let questions = [
+      {
+        type: 'checkbox',
+        name: 'features',
+        message: 'What more would you like?',
+        choices: [
+          {
+            name: 'Modernizr',
+            value: 'modernizr',
+            checked: use.modernizr
+          },
+          {
+            name: 'Foundation',
+            value: 'foundation',
+            checked: use.foundation
+          },
+          {
+            name: 'Bootstrap',
+            value: 'bootstrap',
+            checked: use.bootstrap
+          },
+          {
+            name: 'Backbone',
+            value: 'backbone',
+            checked: use.backbone
+          },
+          {
+            name: 'Marionette',
+            value: 'marionette',
+            checked: use.marionette
+          },
+          {
+            name: 'Sass',
+            value: 'sass',
+            checked: use.sass
+          },
+          {
+            name: 'Cache-crusher',
+            value: 'crusher',
+            checked: use.crusher
+          }
+        ]
+      }
+    ];
+    return this.prompt(questions)
+      .then(answers => {
+        let hasFeature = feat => answers.features.indexOf(feat) !== -1;
 
-  configuring: { saveConfig() {
+        use.modernizr = hasFeature('modernizr');
+        use.foundation = hasFeature('foundation');
+        use.bootstrap = hasFeature('bootstrap');
+        use.backbone = hasFeature('backbone');
+        use.marionette = hasFeature('marionette');
+        use.sass = hasFeature('sass');
+        use.crusher = hasFeature('crusher');
+        return config.set('use', use);
+      }
+    );
+  }
+
+  configuring() { 
     this.config.set(this.settings);
     this.config.save();
   }
-},
 
-  writing: {
-    projectFiles() {
-      console.log('copy project files');
-      this._branchCopy('root');
-    },
-    serverFiles() {
-      console.log('copy server files');
-      let dirs = this.config.get('dirs');
-      this._branchCopy('server', dirs.serverSrc);
-    },
-    clientfiles() {
-      console.log('copy client files');
-      let dirs = this.config.get('dirs'); 
-      this._branchCopy('client', dirs.clientSrc);
-    },
-    testfiles() {
-      console.log('copy test files');
-      let dirs = this.config.get('dirs'); 
-      this._branchCopy('test', dirs.test);
-    }
-  },
+  writing() {
+    const use = this.config.get('use');
+    const dirs = this.config.get('dirs');
+
+    // this.log('copy project files');
+    this._branchCopy({
+      source: 'root',
+      branches: use,
+    });
+
+    // this.log('copy server files');
+    this._branchCopy({
+      source: 'server', 
+      target: dirs.serverSrc,
+      branches: use,
+    });
+
+    // this.log('copy client files');
+    this._branchCopy({
+      source: 'client', 
+      target: dirs.clientSrc,
+      branches: use,
+    });
+
+    // this.log('copy test files');
+    this._branchCopy({
+      source: 'test', 
+      target: dirs.test,
+      branches: use,
+    });
+  }
+
   install() {
     if (!this.options['skip-install']) {
       this.installDependencies();
     }
   }
-});
+};
  
-export default Generator;
+export default AppGenerator;
--- src/lib/base.js
+++ src/lib/base.js
@@ -1,51 +1,38 @@
 import path from 'path';
-import _ from 'lodash';
-import slug from 'slug';
 import yeoman from 'yeoman-generator';
 import ejs from 'ejs';
 
 import BranchFinder from './branch-finder';
 
 
-let BaseGenerator = yeoman.Base.extend({
+class BaseGenerator extends yeoman.Base {
 
-  _getBranches() {
-    let use = this.config.get('use');
+  constructor(...args) {
+    super(...args);
+  }
+
+  _make_template_transformer() {
     return {
-      modernizr: use.modernizr,
-      foundation: use.foundation,
-      bootstrap: use.bootstrap,
-      backbone: use.backbone,
-      marionette: use.marionette,
-      sass: use.sass,
-      crusher: use.crusher,
+      name: 'template',
+      src: /^%(.*)/,
+      tgt: '$1',
+      fn: s => {
+        return ejs.render(s, this.config.getAll());
+      }
     };
-  },
-
-  _transformerMakers: {
-    template() {
-      return {
-        name: 'template',
-        src: /^%(.*)/,
-        tgt: '$1',
-        fn: s => {
-          return ejs.render(s, this.config.getAll());
-        }
-      };
-    }
-  },
+  }
 
   _getTransformers(names) {
     let transformers = [];
-    for (let i = 0; i < names.length; i++) {
-      let name = names[i];
-      transformers.push(this._transformerMakers[name].apply(this));
+    for (const name of names) {
+      const makerName = `_make_${name}_transformer`;
+      const maker = this[makerName];
+      transformers.push(maker.apply(this));
     }
     return transformers;
-  },
+  }
 
   _copyOp(srcPath, tgtPath, transFns) {
-    // console.log "op srcPath=#{srcPath} transFns=#{transFns}"
     let copyOptions = {};
     if (transFns && transFns.length) {
       copyOptions.process = function(contents) {
@@ -57,101 +44,29 @@ let BaseGenerator = yeoman.Base.extend({
         return s;
       };
     }
-
     this.fs.copy(srcPath, tgtPath, copyOptions);
-  },
+  }
 
-  _branchCopy(source, target, pattern) {
-    // console.log "_branchCopy source=#{source} target=#{target} pattern=#{pattern}"
-    let transNames = ['template'];
-    let srcBase = this.sourceRoot();
-    let tgtBase = target || '';
-    pattern = pattern || '**/*';
+  _branchCopy(opt) {
+    const source = opt.source;
+    const tgtBase = opt.target || '';
+    const transNames = opt.transNames || ['template'];
+    const pattern = opt.pattern || '**/*';
+    const branches = opt.branches || {};
+    const srcBase = this.sourceRoot();
     let _options = { 
       pattern: path.join(source, pattern),
       tgtRelalative: source,
-      branches: this._getBranches(),
+      branches: branches,
       transformers: this._getTransformers(transNames),
       op: this._copyOp.bind(this)
     };
     let bf = new BranchFinder(srcBase, tgtBase, _options);
     bf.run();
-  },
-
-  _initConfig() {
-    let { config } = this;
-    config.defaults({
-      appname: this.appname,
-      appnameSlug: slug(this.appname),
-      appnameCap: _.capitalize(this.appname),
-      dirs: {},
-      use: {}});
-  },
-
-  _optionConfig() {
-    let { config } = this;
-    let { options } = this;
-    let dirs = config.get('dirs');
-    config.set('dirs', _.defaults(dirs, {
-      bower: options['bower-dir'],
-      src: options['src-dir'],
-      serverSrc: options['server-src-dir'],
-      clientSrc: options['client-src-dir'],
-      test: options['test-dir'],
-      tmp: options['tmp-dir'],
-      tmpDev: options['tmp-dev-dir'],
-      tmpTest: options['tmp-test-dir'],
-      dist: options['dist-dir']
-    }));
-    //TODO: set 'use'
-  },
-
-  _defaultConfig() {
-    let { config } = this;
-    let dirs = undefined;
-    let use = undefined;
-    dirs = config.get('dirs');
-    config.set('dirs', _.defaults(dirs, {
-      bower: 'bower_components',
-      src: 'src',
-      test: 'test',
-      tmp: '.tmp',
-      dist: 'dist'
-    }
-    )
-    );
-    dirs = config.get('dirs');
-    config.set('dirs', _.defaults(dirs, {
-      serverSrc: path.join(dirs.src, 'server'),
-      clientSrc: path.join(dirs.src, 'client'),
-      tmpDev: path.join(dirs.tmp, 'dev'),
-      tmpTest: path.join(dirs.tmp, 'test')
-    }
-    )
-    );
-    use = config.get('use');
-    config.set('use', _.defaults(use, {
-      backbone: true,
-      marionette: true,
-      foundation: true,
-      bootstrap: false,
-      modernizr: true,
-      sass: true,
-      crusher: true
-    }
-    )
-    );
-  },
-
-  constructor() {
-    yeoman.Base.apply(this, arguments);
-    this.pkg = require('../../package.json');
-    this._initConfig();
-    this._optionConfig();
-    this._defaultConfig();
-    // console.log "..config=#{JSON.stringify(@config.getAll())}"
   }
-});
+
+
+};
 
 
 export default BaseGenerator;
