630a1ad29790caceb49372111da75e634318e9e7
Andreas Riedl <office@tapirdata.com>
2017-05-09 22:31:09 +0200

    start dev server as child process
    make all server changes visible by dodging server cache

--- templates/app/src/scripts/server/start.js
+++ templates/app/src/scripts/server/start.js
@@ -4,16 +4,79 @@ import http from 'http';
 import setupFactory from './setup';
 import appFactory from './app';
 
-export default function(appOptions = {}) {
-  const defaultOptions = setupFactory();
-  const options = _.defaultsDeep({}, appOptions, defaultOptions.app);
-  const app = appFactory(options);
-  return new Promise(function(resolve) {
-    const server = http.createServer(app);
-    server.listen(options.port, function() {
-      // eslint-disable-next-line no-console
-      console.log(`Express server listening on port ${options.port}`);
-      resolve(server);
+
+class Keeper {
+  constructor() {
+  }
+
+  create(options) {
+    const defaultOptions = setupFactory();
+    this.options = _.defaultsDeep({}, options, defaultOptions.app);
+    let app = appFactory(this.options);
+    this.server = http.createServer(app);
+  }
+
+  shout() {
+    // eslint-disable-next-line no-console
+    console.log(`Server listening on port ${this.options.port}`);
+  }
+
+  start() {
+    this.server.listen(this.options.port);
+  }
+
+  startSafe() {
+    return new Promise((resolve, reject) => {
+      this.server.on('listening', () => {
+        this.shout();
+        resolve(this.server);
+      });
+      this.server.on('error', (err) => {
+        reject(err);
+      });
+      this.start();
     });
-  });
+  }
+
+  attach() {
+    this.server.on('listening', () => {
+      this.shout();
+      process.send({state: 'started'});
+    });
+    this.server.on('close', () => {
+      process.send({state: 'stopped'});
+    });
+    this.server.on('error', (err) => {
+      process.send({state: 'failed', error: err});
+    });
+  }
+
+  wait() {
+    process.on('message', (message) => {
+      switch(message.cmd) {
+      case 'start':
+        this.create(message.options);
+        this.attach();
+        this.start();
+        break;
+      case 'stop':
+        this.server.close();
+        break;
+      }
+    });
+  }
+
 }
+
+if (require.main === module) {
+  let keeper = new Keeper();
+  keeper.wait();
+}
+
+function start(options = {}) {
+  let keeper = new Keeper();
+  keeper.create(options);
+  return keeper.startSafe();
+}
+
+export default start;
